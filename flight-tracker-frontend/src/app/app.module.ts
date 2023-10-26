3cbfc7fd95f64cd09df16f83ddb45fa67be79078
Bing <bing334@gmail.com>
2023-10-26 19:29:05 -0400

    Generate dashboard component. Define routes

--- flight-tracker-frontend/src/app/app.module.ts
+++ flight-tracker-frontend/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FlightInfoComponent } from './components/flight-info/flight-info.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path: 'flight-info', component: FlightInfoComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch:'full'},
+  {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlightInfoComponent
+    FlightInfoComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
