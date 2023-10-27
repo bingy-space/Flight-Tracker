89cbd6ae7fe6df7086df02af62bf07b309da09f5
Bing <bing334@gmail.com>
2023-10-26 20:37:42 -0400

    Update routes and routerLinks. Delete flight-info component

--- flight-tracker-frontend/src/app/app.module.ts
+++ flight-tracker-frontend/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { FlightInfoComponent } from './components/flight-info/flight-info.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AirlineComponent } from './components/airline/airline.component';
+import { AllFlightInfoComponent } from './components/all-flight-info/all-flight-info.component';
+import { EditFlightInfoComponent } from './components/edit-flight-info/edit-flight-info.component';
+import { NewFlightInfoComponent } from './components/new-flight-info/new-flight-info.component';
 
 const routes: Routes = [
-  {path: 'flight-info', component: FlightInfoComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'new-flight-info', component: NewFlightInfoComponent},
+  {path: 'edit-flight-info', component: EditFlightInfoComponent},
+  {path: 'all-flight-info', component: AllFlightInfoComponent},
+  {path: 'airline', component: AirlineComponent},
   {path: '', redirectTo: '/dashboard', pathMatch:'full'},
   {path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
@@ -16,8 +22,11 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    FlightInfoComponent,
-    DashboardComponent
+    DashboardComponent,
+    AirlineComponent,
+    AllFlightInfoComponent,
+    EditFlightInfoComponent,
+    NewFlightInfoComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
