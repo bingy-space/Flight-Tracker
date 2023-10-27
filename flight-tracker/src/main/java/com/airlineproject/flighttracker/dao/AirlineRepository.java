d1fb0e40827e7b613b2b73116b040c4b73cea8b3
Bing <bing334@gmail.com>
2023-10-27 18:53:42 -0400

    Add CrossOrigin

--- flight-tracker/src/main/java/com/airlineproject/flighttracker/dao/AirlineRepository.java
+++ flight-tracker/src/main/java/com/airlineproject/flighttracker/dao/AirlineRepository.java
@@ -2,9 +2,11 @@ package com.airlineproject.flighttracker.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import com.airlineproject.flighttracker.entity.Airline;
 
-
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "airlines", path="airlines")
 public interface AirlineRepository extends JpaRepository<Airline, Integer>{
 
--- flight-tracker/src/main/java/com/airlineproject/flighttracker/dao/FlightInfoRepository.java
+++ flight-tracker/src/main/java/com/airlineproject/flighttracker/dao/FlightInfoRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.airlineproject.flighttracker.entity.Flight_Info;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource
 public interface FlightInfoRepository extends JpaRepository<Flight_Info, Long>{
 	Page<Flight_Info> findByFlightCodeContaining(@Param("flightCode") String flightCode, Pageable pageable);
