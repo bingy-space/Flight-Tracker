450b42af16cc54375a0e7aeed84b0bc1f47c73a1
Bing <bing334@gmail.com>
2023-10-31 18:08:43 -0400

    Create and develop flight-info service to call REST API
    - getFlightStatusInfo()

---
+++ flight-tracker-frontend/src/app/services/flight-info.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { FlightInfo } from '../common/flight-info';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightInfoService {
+
+  private baseUrl = 'http://localhost:8080/api/flight_Infoes';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getFlightStatusInfo(): Observable<FlightInfo[]>{
+    return this.httpClient.get<GetResponseFlightInfo>(this.baseUrl).pipe(
+      map(response => response._embedded.flight_Infoes)
+    )
+  }
+
+}
+interface GetResponseFlightInfo{
+  _embedded: {
+    flight_Infoes: FlightInfo[];
+  }
+}
