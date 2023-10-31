54dcd6d6e776f107302a4142d65c9ed390296f69
Bing <bing334@gmail.com>
2023-10-31 18:06:15 -0400

    Develop FlightInfo class

---
+++ flight-tracker-frontend/src/app/common/flight-info.ts
@@ -0,0 +1,21 @@
+export class FlightInfo {
+    // id: number;
+    // airline: string;
+    // flight: string;
+    // status: string;
+    // gate: string;
+    // flightCode: string;
+    // airPortCode: string;
+    // scheduledTime: string;
+    // updateTime: string;
+
+    constructor(public id: number,
+        public airline: string,
+        public flight: string,
+        public status: string,
+        public gate: string,
+        public flightCode: string,
+        public airportCode: string,
+        public scheduledTime: string,
+        public updateTime: string){}
+}
