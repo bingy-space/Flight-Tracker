920caa1598defb730b1ae36a3e7476431702da4f
Bing <bing334@gmail.com>
2023-11-03 20:41:39 -0400

    Add method to convert airport code to airport city from JSON file
    -getAirportCityByCode(airportCode: any)

--- flight-tracker-frontend/src/app/components/all-flight-info/all-flight-info.component.ts
+++ flight-tracker-frontend/src/app/components/all-flight-info/all-flight-info.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FlightInfo } from 'src/app/common/flight-info';
 import { FlightInfoService } from 'src/app/services/flight-info.service';
+import * as airportInfo from './../../../assets/airports.json';
 
 @Component({
   selector: 'app-all-flight-info',
@@ -9,11 +10,13 @@ import { FlightInfoService } from 'src/app/services/flight-info.service';
 })
 export class AllFlightInfoComponent {
   flightStatusInfo: FlightInfo[] = [];
+  airportInfoData: any = airportInfo;
 
   constructor(private flightInfoService: FlightInfoService){}
 
   ngOnInit(): void{
     this.listFlightStatusInfo();
+    // this.getAirportCityByCode();
   }
 
   // List of flight status info
@@ -21,9 +24,24 @@ export class AllFlightInfoComponent {
     this.flightInfoService.getFlightStatusInfo().subscribe(
       data => {
         this.flightStatusInfo = data;
-        console.log(data)
+        // Converting airport code to airport city name
+        this.flightStatusInfo[0].airportCode = this.getAirportCityByCode(this.flightStatusInfo);
       }
     )
+  }
+
+  // Get airport city name by airport code from JSON
+  // Param: airportCode
+  getAirportCityByCode(airportCode: any){
+    let airportCodeTemp = airportCode[0].airportCode;
+
+    for(let i = 0; i <= this.airportInfoData.length; i++){
+      if(this.airportInfoData[i]['iata_code'] == airportCodeTemp){
+        console.log('Find it')
+        console.log(this.airportInfoData[i].city)
+        return this.airportInfoData[i].city;
+      }
+    }
   }
 
 
