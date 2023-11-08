67c5c3609fe47af2f0114de4e0699b8c10329aba
Bing <bing334@gmail.com>
2023-11-07 21:11:03 -0500

    Add CrossOrigin

--- flight-tracker/src/main/java/com/airlineproject/flighttracker/dao/GateRepository.java
+++ flight-tracker/src/main/java/com/airlineproject/flighttracker/dao/GateRepository.java
@@ -2,9 +2,11 @@ package com.airlineproject.flighttracker.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.airlineproject.flighttracker.entity.Gate;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "gates", path="gates")
 public interface GateRepository extends JpaRepository<Gate, Integer>{
 
--- flight-tracker/src/main/java/com/airlineproject/flighttracker/dao/StatusRepository.java
+++ flight-tracker/src/main/java/com/airlineproject/flighttracker/dao/StatusRepository.java
@@ -2,9 +2,11 @@ package com.airlineproject.flighttracker.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.airlineproject.flighttracker.entity.Status;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "status", path="status")
 public interface StatusRepository extends JpaRepository<Status, Integer>{
 
